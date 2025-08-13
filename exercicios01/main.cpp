0b6687f9df8ec227bd560e0a8a57e1dd2dcb514a
psicoiner <migueltsilva71@gmail.com>
2025-08-13 11:30:35 -0300

    feat(cpp): primeiros exercícios praticando cout

---
+++ exercicios01/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "0 - Fim\n";
+    cout << "1 - Inclui\n";
+    cout << "2 - Altera\n";
+    cout << "3 - Exclui\n";
+    cout << "4 - Consulta";
+    return 0;
+}
