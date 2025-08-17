70012abf353a66439bafeb742052a130b8ffb7b2
psicoiner <migueltsilva71@gmail.com>
2025-08-17 16:52:25 -0300

    feat(cpp): aprender variáveis e tipos em C++

---
+++ variaveis-tipos/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool boleano;
+    boleano = true;
+
+    cout <<"Verdadeiro no C++ e: " <<boleano <<endl;
+
+    boleano = false;
+
+    cout <<"Falso no C++ e: " <<boleano <<endl;
+
+    return 0;
+}
