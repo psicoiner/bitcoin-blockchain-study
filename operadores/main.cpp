64dc4115b23295aa0546da819d874eeef93635a5
psicoiner <migueltsilva71@gmail.com>
2025-08-21 12:11:52 -0300

    Estudo: operadores matemáticos em C++

---
+++ operadores/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<21 + 12 <<endl;
+    short valor1 = 1, valor2 = 4;
+    cout <<valor1 + valor2 <<endl;
+
+    short valor3 = 2, valor4 = 5;
+    cout <<"21 - 12 = " <<21 - 12 <<endl;
+    cout <<"2 - 5 = " <<valor3 - valor4 <<endl;
+
+    short valor5 = 2, valor6 = 2;
+    cout <<"2 * 2 = " <<valor5 * valor6 <<endl;
+    cout <<"21 * 12 = " <<21 * 12 <<endl;
+
+    float valor7 = 1, valor8 = 3;
+    cout <<"4 / 2 = " <<4 / 2 <<endl;
+    cout <<"1 / 3 = " <<valor7 / valor8 <<endl;
+
+    cout <<"Atividades:" <<endl;
+
+    cout <<10 + 20 + 30 <<endl;
+    float var1 = 21, var2 = 2;
+    cout <<var1 / var2 <<endl;
+    cout <<21 % 12 <<endl;
+    short soma = 10 + 20 +30;
+    cout <<soma / 3 <<endl;
+    cout <<(10 + 20 + 30) / 3 <<endl;
+    return 0;
+}
