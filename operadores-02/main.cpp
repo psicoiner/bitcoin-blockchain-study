5622c362a31437270ff3dc5f3afe2979359a0372
psicoiner <migueltsilva71@gmail.com>
2025-09-01 22:21:14 -0300

    Estudo: operadores matemáticos em C++

---
+++ operadores-02/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "1+6*3=" << 1+6*3 << endl;
+    cout << "1+6/2=" << 1+6/2 << endl;
+    cout << "(1+6)*3=" << (1+6)*3 << endl;
+    return 0;
+}
