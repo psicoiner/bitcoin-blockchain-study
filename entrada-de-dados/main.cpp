91a8a4a6f3a307e388a8a3931e273297598eed23
psicoiner <migueltsilva71@gmail.com>
2025-09-02 23:36:07 -0300

    Estudo: introdução ao comando de entrada cin em C++

---
+++ entrada-de-dados/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool val;
+
+    cout << "Digite algo: ";
+    cin >> val;
+    cout << "Isso representa o booleano: " << val;
+
+    return 0;
+}
